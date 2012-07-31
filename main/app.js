ff5bb51fe0e948ec76501aa2c6e1ece1f98904c0
ashwath <aps398@nyu.edu>
2012-07-31 16:19:45 -0400

    Part-2, Generating actor's not followed by User, for each tip

--- main/app.js
+++ main/app.js
@@ -8,19 +8,47 @@ var tips = new TipsClient({
 
 var foreach = function(obj, func) {
 	for(var i = 0; i < obj.length; i++) { 
-		func(obj[i], i); // execute a function and make the obj, objIndex available
+		func(obj[i]); // execute a function and make the obj, objIndex available
 	}
 }
 
+var user_id = '4f84a219b04953bc34000220';
 
-tips.user.tips('Ashwath', function(error, ret){
-	if(error) {
+tips.user.followers(user_id, function(error, ret){
+	if(error){
 		console.log(error);
 	}
-	else {
-		foreach(ret.tips, function(tip){
-			product.tipsters(tip.head, console.log);
+	else{
+		var hash = {};
+		foreach(ret.followers, function(actor){
+			hash[actor.head] = actor;
+		});
+		
+		tips.user.tips(user_id, function(error, ret1){
+			if(error) {
+				console.log(error);
+			}
+			else {
+				var product_list = ret1.tips;
+				foreach(ret1.tips, function(tip){
+					product.tipsters(tip.head, function(error, ret2){
+						if(error) {
+							console.log(error);
+						}
+						else {
+							var tipster_list = [];
+							foreach(ret2.tipped_by, function(actor1){							
+								if(!hash[actor1.head]){
+									tipster_list.push(actor1);
+								}	
+							});
+							return( product_list,tip,tipster_list);
+						}
+					}); 
+					
+				});
+				
+			}
 		});
 	}
-
 });
