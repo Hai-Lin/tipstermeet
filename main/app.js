8849f034bc75d54869e019390fc5578e58948332
ashwath <aps398@nyu.edu>
2012-08-01 10:45:42 -0400

    Preparation for integration of earlier code with server

--- main/app.js
+++ main/app.js
@@ -1,62 +1,67 @@
-var TipsClient = require('../');
-var product = require('../lib/product');
-var tips = new TipsClient({
-  //key: '932a4e6f898648eca95ee2920fb99c2f',
-  //secret: '9dd49dc4c9ac4657a0a65605f2c27657',
-  key:'8ba43918d5b04a71aef489de9e2a88b0',
-  secret:'72400e24cca646e6bb56432e16a4ce62',
-  debug: true
-});
+function start(){
+	var TipsClient = require('../');
+	var product = require('../lib/product');
+	var tips = new TipsClient({
+  		//key: '932a4e6f898648eca95ee2920fb99c2f',
+  		//secret: '9dd49dc4c9ac4657a0a65605f2c27657',
+  		key:'8ba43918d5b04a71aef489de9e2a88b0',
+  		secret:'72400e24cca646e6bb56432e16a4ce62',
+  		debug: true
+	});
 
-var foreach = function(obj, func) {
-	for(var i = 0; i < obj.length; i++) { 
-		func(obj[i],i); // execute a function and make the obj, objIndex available
+	var foreach = function(obj, func) {
+		for(var i = 0; i < obj.length; i++) { 
+			func(obj[i],i); // execute a function and make the obj, objIndex available
+		}
 	}
+
+	var user_id = '4f7723ddf5d16f4f20000295';
+
+	tips.user.followers(user_id, function(error, ret){
+		if(error){
+			console.log(error);
+		}
+		else{
+			var hash = {},
+				tipster_list = [],
+				final_list = {},
+				product_list = {}
+			;
+
+			foreach(ret.followers, function(actor){
+				hash[actor.head] = actor;
+			});
+		
+			tips.user.tips(user_id, function(error, ret1){
+				if(error) {
+					console.log(error);
+				}
+				else {
+					product_list = ret1.tips;
+					foreach(ret1.tips, function(tip,i){
+						product.tipsters(tip.head, function(error, ret2){
+							if(error) {
+								console.log(error);
+							}
+							else {
+								tipster_list = [];
+								foreach(ret2.tipped_by, function(actor1){							
+									if(!hash[actor1.head]){
+										tipster_list.push(actor1);
+									}	
+								});
+								final_list[tip.head] = tipster_list;							
+							}
+							if(!ret1.tips[++i]) {
+								return [final_list,product_list];		
+							}
+						}); 
+					});
+				}
+			});
+		}
+	});
+
 }
 
-var user_id = '4f7723ddf5d16f4f20000295';
-
-tips.user.followers(user_id, function(error, ret){
-	if(error){
-		console.log(error);
-	}
-	else{
-		var hash = {},
-			tipster_list = [],
-			final_list = {},
-			product_list = {}
-		;
-
-		foreach(ret.followers, function(actor){
-			hash[actor.head] = actor;
-		});
-		
-		tips.user.tips(user_id, function(error, ret1){
-			if(error) {
-				console.log(error);
-			}
-			else {
-				product_list = ret1.tips;
-				foreach(ret1.tips, function(tip,i){
-					product.tipsters(tip.head, function(error, ret2){
-						if(error) {
-							console.log(error);
-						}
-						else {
-							tipster_list = [];
-							foreach(ret2.tipped_by, function(actor1){							
-								if(!hash[actor1.head]){
-									tipster_list.push(actor1);
-								}	
-							});
-							final_list[tip.head] = tipster_list;							
-						}
-						if(!ret1.tips[++i]) {
-							return [final_list,product_list];		
-						}
-					}); 
-				});
-			}
-		});
-	}
-});
+exports.start= start;
